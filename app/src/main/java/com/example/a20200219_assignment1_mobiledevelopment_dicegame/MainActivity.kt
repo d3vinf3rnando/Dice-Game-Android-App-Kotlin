0ae1fd4260f393979348414bf317d9b373f72b7c
Devin Fernando <103265400+d3vinf3rnando@users.noreply.github.com>
2023-03-15 19:54:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/a20200219_assignment1_mobiledevelopment_dicegame/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a20200219_assignment1_mobiledevelopment_dicegame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
